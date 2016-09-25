44b919c37104ac9be6a13df6f0f87f8790a10a96
inemuuuri <inemuuuri@gmail.com>
2016-09-25 17:00:12 +0900

    0925_02

--- src/jp/tnw/a18/Graphics.java
+++ src/jp/tnw/a18/Graphics.java
@@ -1,5 +1,6 @@
 package jp.tnw.a18;
 
+import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -9,8 +10,8 @@ import javax.swing.JFrame;
 
 public abstract class Graphics {
 	// // 画像の容器
-	//BufferedImage image;
-	//BufferedImage image[] = new BufferedImage[XX];
+	// BufferedImage image;
+	// BufferedImage image[] = new BufferedImage[XX];
 	// // 表示するユニットの最大の数
 	// int UNIT_MAX = 1000;
 	// // 画像の切り替え用の変数
@@ -41,7 +42,7 @@ public abstract class Graphics {
 	// double hitCir[] = new double[UNIT_MAX];
 	// double hitBoxW[] = new double[UNIT_MAX];
 	// double hitBoxH[] = new double[UNIT_MAX];
-	Graphics(){
+	Graphics() {
 		// for (int i = 0; i < UNIT_MAX; i++) {
 		//
 		// isVisible[i] = false;
@@ -71,63 +72,48 @@ public abstract class Graphics {
 		//
 		// }
 	}
-	
-	public void loadImage(String filename, BufferedImage image[] , int fileqty) {
+
+	public void loadImage(BufferedImage image, String filename) {
 
 		try {
-			if (fileqty > 0 || fileqty <= 10000) {
-
-				// ファイルが一つしかいない場合
-				if (fileqty == 1) {
-
-					image[0] = ImageIO.read(getClass().getResource(filename + ".png"));
-
-				} else {
-					String num = "";
-					for (int i = 0; i < fileqty; i++) {
-
-						// ファイル名のゼロの処理
-						// ファイル名例：gazou_0000.png gazou_0032.png gazou_9999.png
-						if (i < 10) {
-							num = "000" + i;
-						} else if (i < 100) {
-							num = "00" + i;
-						} else if (i < 1000) {
-							num = "0" + i;
-						}
-
-						image[i] = ImageIO.read(getClass().getResource(filename + "_" + num + ".png"));
-
-					}
-
-				}
-
-			}
-
-		} catch (IOException e) {
-
-			// ファイルエラーの場合
-			e.printStackTrace();
-
-		}
-	}
-	
-	
-	public void loadImage(String filename, BufferedImage image){
-		
-		try {
-
 			image = ImageIO.read(getClass().getResource(filename + ".png"));
-
 		} catch (IOException e) {
-
 			// ファイルエラーの場合
 			e.printStackTrace();
-
 		}
-		
-	}
-	
 
-	
+	}
+
+	public void drawImage(Graphics2D g, JFrame window, BufferedImage image, int widthBlock, int heightBlock, int UNIT_MAX,
+			int imageIndex, float opacity, double dX, double dY) {
+
+		// 一コマの幅をゲット
+		int blockW = image.getWidth() / widthBlock;
+		// 一コマの高さをゲット
+		int blockH = image.getHeight() / heightBlock;
+		// 描画したいコマの左上端座標をゲット
+		int indexX = (imageIndex % widthBlock == 0) ? blockW * (widthBlock - 1)
+				: blockW * ((imageIndex % widthBlock) - 1);
+		int indexY = (imageIndex % widthBlock == 0) ? blockH * (imageIndex / widthBlock - 1)
+				: blockH * (imageIndex / widthBlock);
+		// 不透明度
+		g.setComposite((AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity)));
+		// 描画
+		g.drawImage(image,
+				// 画面に出したいところ
+				(int) dX, // 左上端X座標
+				(int) dY, // 左上端Y座標
+				(int) dX + blockW, // 右下端X座標
+				(int) dY + blockH, // 右下端Y座標
+				// 画像のどこを使う
+				indexX, // 左上端X座標
+				indexY, // 左上端Y座標
+				indexX + blockW, // 右下端X座標
+				indexY + blockH, // 右下端Y座標
+				window);
+		// 不透明度リセット
+		g.setComposite((AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f)));
+
+	}
+
 }
